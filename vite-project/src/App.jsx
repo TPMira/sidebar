2f1b314f80ccf9e0b449c2650882f9ec788c7641
thiagodepaulamira <thiagodepaulamira@gmail.com>
2022-06-21 14:36:44 -0300

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
@@ -19,9 +20,11 @@ const App = () => {
         <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
             <div className="fixed right-4 bottom-4" style={{zIndex: '1000' }}>
-                <button type='button' className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue', borderRadius: '50%'}}>
-                    <FiSettings/>
-                </button>
+                <TooltipComponent content='Settings' position='Top'>
+                    <button type='button' className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue', borderRadius: '50%'}}>
+                        <FiSettings/>
+                    </button>
+                </TooltipComponent>
             </div>
             {activeMenu ? (
                 <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
@@ -36,7 +39,7 @@ const App = () => {
                 <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                     <Navbar/>
                 </div>
-            </div>
+            
 
             <div>
                 <Routes>
@@ -66,7 +69,7 @@ const App = () => {
                     <Route path="/stacked" element={<Stacked />} />
                 </Routes>
             </div>
-
+            </div>
         </div>
         </BrowserRouter>
     </div>
--- vite-project/src/pages/Orders.jsx
+++ vite-project/src/pages/Orders.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Header } from '../components'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category='Page' title='Orders' />
+    </div>
   )
 }
 
